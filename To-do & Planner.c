58a065c2bed77d1f3173a618f4fb443aba622788
Vikram <59924856+justVikram@users.noreply.github.com>
2021-02-21 13:36:39 +0530

    Added calendar functions

--- To-do & Planner.c
+++ To-do & Planner.c
@@ -3,6 +3,7 @@
 
 typedef struct Subtask
 {
+    int SubtaskNumber;
     char SubtaskTitle [40];
     int IsCompleted;
 }
@@ -63,6 +64,7 @@ static void readNewTask (TASK_NODE * NewTask)
     
 }
 
+
 /// Adds the new task created by the user to the list of tasks being maintained.
 /// @param ListOfTasks Pointer pointing to the address of the original list of tasks
 /// @param NewTask After readNewTask(), pass NewTask to this function.
@@ -114,6 +116,7 @@ static void deleteTaskFromList (TASK_NODE ** ListOfTasks, char TaskTitle [])
     
 }
 
+
 //MARK:-
 
 static void readNewSubtask (SUBTASK_NODE * NewSubtask)
@@ -153,14 +156,65 @@ static void deleteSubtaskList (TASK_NODE ** ExistingTask)
 
 //MARK:-
 
-static void displayCalendar ()
+int isLeapYear (int y)
 {
-    //USE ZELLER'S ALGORITHM
+    return(y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0));
+}
+
+int leapYears (int y)
+{
+    return y/4 - y/100 + y/400;
+}
+
+int todayOf (int y, int m, int d)
+{
+    static int DayOfMonth[] = {-1, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+    return DayOfMonth[m] + d + ((m > 2 && isLeapYear (y))? 1 : 0);
+}
+
+long days (int y, int m, int d)
+{
+    int lastYear;
+    lastYear = y - 1;
+    
+    return 365L * lastYear + leapYears(lastYear) + todayOf(y,m,d);
+}
+
+void calendar (int y, int m) /* display calendar at m y */
+{
+    const char *NameOfMonth[] = {NULL, "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"};
+    
+    char Week[] = "Su Mo Tu We Th Fr Sa";
+    
+    int DayOfMonth[] = { -1,31,28,31,30,31,30,31,31,30,31,30,31 };
+    int weekOfTopDay;
+    int i, day;
+
+    weekOfTopDay = days(y, m, 1) % 7;
+    
+    if (isLeapYear (y))
+        DayOfMonth [2] = 29;
+    
+    printf ("\n     %s %d\n%s\n", NameOfMonth[m], y, Week);
+
+    for (i = 0; i < weekOfTopDay; i++)
+        printf("   ");
+    
+    for (i = weekOfTopDay, day = 1; day <= DayOfMonth [m]; i++, day++)
+    {
+        printf ("%2d ", day);
+        
+        if(i % 7 == 6)
+            printf("\n");
+    }
+    printf("\n");
 }
 
 //MARK:-
 
-int main (int argC, const char * argV [])
+int main (int argV, const char * argC [])
 {
     TASK_NODE * ListOfTasks;
 }
